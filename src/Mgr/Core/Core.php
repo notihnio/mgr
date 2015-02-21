428009ea0b65f0b471b5c3fd7e9054638d369123
Panagiotis Mastrandrikos <pmastrandrikos@gmail.com>
2015-02-21 18:16:39 +0200

    Each module supports local bootstrap class
    -2 events added: module.preDispach, modulePostdispach

--- src/Mgr/Core/Core.php
+++ src/Mgr/Core/Core.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
-    @author Panagiotis Mastrandrikos <pmastrandrikos@gmail.com>  https://github.com/notihnio
- 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
- 
+  @author Panagiotis Mastrandrikos <pmastrandrikos@gmail.com>  https://github.com/notihnio
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
  */
 
 namespace Mgr\Core;
@@ -19,9 +19,8 @@ namespace Mgr\Core;
  * @description Does everything - is the whole app
  * @author notihnio
  */
-
 class Core {
-    
+
     /**
      *
      * @type Array
@@ -29,70 +28,92 @@ class Core {
      * @description handles the configuration;
      */
     private $configuration;
-    
-    
+
     /**
      *
      * @type Array
      * @name routes 
      * @description RouterRoutes;
      */
-    private $routes;
-    
-    
-        
-    
+    private $routes = array();
+
     /**
      * @type Array
      * @name $selectedRoute 
      * @description The selected route from route - initialization after router despach;
      */
-    public  $selectedRoute = null;
-    
-    
-            
+    public $selectedRoute = null;
+
     function __construct() {
-        $this->configuration = \Application\Config\Configurator::config();
-        $this->routes = \Application\Config\Routes::routes();
+        $this->configuration = \Application\Config\Configurator::config();   
+        
+        //check for local route file in module directories
+        $path = ROOT.DIRECTORY_SEPARATOR."Application".DIRECTORY_SEPARATOR."Module"; // '.' for current
+        foreach (new \DirectoryIterator($path) as $file) {
+            if ($file->isDot())
+                continue;
+
+            if ($file->isDir()) {
+                $moduleName = $file->getFilename();
+                
+                //check if Module/Config/Routes file exists
+                $moduleRoutesClassPath = ROOT.DIRECTORY_SEPARATOR."Application".DIRECTORY_SEPARATOR."Module".DIRECTORY_SEPARATOR.$moduleName.DIRECTORY_SEPARATOR."Config".DIRECTORY_SEPARATOR."Routes.php";
+                if(file_exists($moduleRoutesClassPath)){
+                    //get module local routes
+                    $moduleRouteClassName =  "\\Application\\Module\\{$moduleName}\\Config\\Routes";
+                    $moduleRoutes = $moduleRouteClassName::routes();
+                    $this->routes = array_merge($this->routes, $moduleRoutes);
+                }
+                 
+            }
+        }
+       
+        //add global routes
+        $this->routes = array_merge($this->routes, \Application\Config\Routes::routes());
     }
-    
-    
-    function dispach(){
-        try{
-            
+
+    function dispach() {
+        try {
+
             //init Router
             $router = new \Mgr\Router\Router($this->routes, $this->configuration);
-            
+
             //dispach router
             \Mgr\Event\Event::trigger("router.preDispach");
             $this->selectedRoute = $router->dispach();
             \Mgr\Event\Event::trigger("router.postDispach", $this->selectedRoute);
-           
+
             //create selected route namespace
-            $selectedRouteNamespace = "\Application\Module\\".ucfirst($this->selectedRoute["module"]);
-            $controllerName = $selectedRouteNamespace."\Controller\\".ucfirst($this->selectedRoute["controller"])."Controller"; 
-            
-            
+            $selectedRouteNamespace = "\Application\Module\\" . ucfirst($this->selectedRoute["module"]);
+            $controllerName = $selectedRouteNamespace . "\Controller\\" . ucfirst($this->selectedRoute["controller"]) . "Controller";
+
+
             //init selected controller
             $controller = new $controllerName($selectedRouteNamespace);
-            $controller->viewFolderPath = ROOT.str_replace("\\", DIRECTORY_SEPARATOR, $selectedRouteNamespace).DIRECTORY_SEPARATOR."View".DIRECTORY_SEPARATOR.ucfirst($this->selectedRoute["controller"]);
+            $controller->viewFolderPath = ROOT . str_replace("\\", DIRECTORY_SEPARATOR, $selectedRouteNamespace) . DIRECTORY_SEPARATOR . "View" . DIRECTORY_SEPARATOR . ucfirst($this->selectedRoute["controller"]);
             $controller->selectedRoute = $this->selectedRoute;
-            $controller->view = new \Mgr\View\View($controller->viewFolderPath.DIRECTORY_SEPARATOR.ucfirst($this->selectedRoute["action"]));
+            $controller->view = new \Mgr\View\View($controller->viewFolderPath . DIRECTORY_SEPARATOR . ucfirst($this->selectedRoute["action"]));
             $controller->getParams = $_GET;
             $controller->postParams = $_POST;
-            $controller->params = isset( $this->selectedRoute["params"])? $this->selectedRoute["params"]: array();
+            $controller->params = isset($this->selectedRoute["params"]) ? $this->selectedRoute["params"] : array();
             //fire The selected action
-            $selectedAction = ucfirst($this->selectedRoute["action"])."Action";
+            $selectedAction = ucfirst($this->selectedRoute["action"]) . "Action";
+
+            //check if module local bootstrap class exists
+            if(file_exists(ROOT.DIRECTORY_SEPARATOR."Application".DIRECTORY_SEPARATOR."Module".DIRECTORY_SEPARATOR.ucfirst($this->selectedRoute["module"]).DIRECTORY_SEPARATOR."Bootstrap.php")){ 
+                $bootstrapClass = $selectedRouteNamespace."\\Bootstrap";
+                // execute go 
+                \Mgr\Event\Event::trigger("module.preDispach");
+                $bootstrapClass::go();
+                \Mgr\Event\Event::trigger("module.postDispach");
+            }
+            
             $controller->$selectedAction();
-            
-        }catch (\Exception $error){
+        } catch (\Exception $error) {
             $error->getMessage();
-            
-        }catch (\Mgr\Exception\Route $error){
+        } catch (\Mgr\Exception\Route $error) {
             $error->getMessage();
-            
         }
-        
-  
     }
+
 }
